173052f78cfe072929d41670f4eafe6bd11e19e3
dangdinhsi <siddd00474@fpt.edu.vn>
2018-10-22 19:45:21 +0700

    Upload example asp.net core

---
+++ BaithiASPdotNet22102018/Data/BaithiASPdotNet22102018Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BaithiASPdotNet22102018.Models;
+
+namespace BaithiASPdotNet22102018.Models
+{
+    public class BaithiASPdotNet22102018Context : DbContext
+    {
+        public BaithiASPdotNet22102018Context (DbContextOptions<BaithiASPdotNet22102018Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BaithiASPdotNet22102018.Models.Category> Category { get; set; }
+
+        public DbSet<BaithiASPdotNet22102018.Models.Product> Product { get; set; }
+    }
+}
---
+++ BaithiASPdotNet22102018/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaithiASPdotNet22102018.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+
+        public string Description { get; set; }
+        public string Picture { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
